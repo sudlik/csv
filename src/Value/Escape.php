aec6a76cc9af0c096ca0354fc9aa17b45498cadb
Marek Sudoł <m.sudol@smsapi.pl>
2015-03-05 16:27:57 +0100

    new escape characters

--- src/Value/Escape.php
+++ src/Value/Escape.php
@@ -5,13 +5,21 @@ namespace Csv\Value;
 use ValueObjects\Enum\Enum;
 
 /**
+ * @method static Escape APOSTROPHE()
+ * @method static Escape QUOTATION_MARK()
+ * @method static Escape GRAVE_ACCENT()
  * @method static Escape BACKSLASH()
  * @method static Escape FORWARD_SLASH()
+ * @method static Escape NONE()
  */
 final class Escape extends Enum
 {
+    const APOSTROPHE = "'";
+    const QUOTATION_MARK = '"';
+    const GRAVE_ACCENT = '`';
     const BACKSLASH = '\\';
     const FORWARD_SLASH = '/';
+    const NONE = '';
 
     public function __toString()
     {
