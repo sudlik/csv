bd9f5beb0244a607eb8e422a9d917d73425f034d
Marek Sudoł <m.sudol@smsapi.pl>
2015-03-05 16:28:23 +0100

    rename enclosure positions

--- src/Value/EnclosurePositions.php
+++ src/Value/EnclosurePositions.php
@@ -5,16 +5,16 @@ namespace Csv\Value;
 use ValueObjects\Enum\Enum;
 
 /**
- * @method static EnclosurePositions ALWAYS()
+ * @method static EnclosurePositions ALWAYS_FORCE()
  * @method static EnclosurePositions STANDARD()
- * @method static EnclosurePositions FRACTION()
+ * @method static EnclosurePositions FORCE_ON_FRACTIONS()
  * @method static EnclosurePositions NONE()
  */
 final class EnclosurePositions extends Enum
 {
     const STANDARD = 'standard';
-    const ALWAYS = 'always';
-    const FRACTION = 'fraction';
+    const ALWAYS_FORCE = 'always';
+    const FORCE_ON_FRACTIONS = 'fraction';
     const NONE = 'none';
 
     public function __toString()
